b9651f01ae57bf1fd0788899794d0ee0c7d38d3d
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-10-31 12:41:56 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://airbnb-clone1-backend.onrender.com/api/",
+  baseURL: "https://airbnb-clone1-backend.onrender.com/api/",
 });
 
 // User signup and signin functions
@@ -10,38 +10,38 @@ export const UserSignIn = async (data) => await API.post("/user/signin", data);
 
 // Get all properties with a filter
 export const getAllProperty = async (filter) =>
-    await API.get(`/property/get?${filter}`); // Fixed string interpolation
+  await API.get(`/property/get?${filter}`);
 
 // Get property details by ID
 export const getPropertyDetails = async (id) =>
-    await API.get(`/property/${id}`);
+  await API.get(`/property/${id}`);
 
 // Get user's favorite properties
 export const getFavourite = async (token) =>
-    await API.get('/user/getFavorites/', {
-        headers: { Authorization: `Bearer ${token}` }, // Fixed string interpolation
-    });
+  await API.get('/user/getFavorites/', {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
 // Add a property to favorites
 export const addToFavourite = async (token, data) =>
-    await API.post('/user/addToFavorites/', data, {
-        headers: { Authorization: `Bearer ${token}` }, // Fixed string interpolation
-    });
+  await API.post('/user/addToFavorites/', data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
 // Remove a property from favorites
 export const deleteFromFavourite = async (token, data) =>
-    await API.post('/user/removeFavorite', data, {
-        headers: { Authorization: `Bearer ${token}` }, // Fixed string interpolation
-    });
+  await API.post('/user/removeFavorite', data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
 // Book a property
 export const bookProperty = async (token, data) =>
-    await API.post('/user/booking/', data, {
-        headers: { Authorization: `Bearer ${token}` }, // Fixed string interpolation
-    });
+  await API.post('/user/booking/', data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
 // Get booked properties
 export const getBookedProperty = async (token) =>
-    await API.get('/user/getBooking/', {
-        headers: { Authorization: `Bearer ${token}` }, // Fixed string interpolation
-    });
+  await API.get('/user/getBooking/', {
+    headers: { Authorization: `Bearer ${token}` },
+  });
