1933e061760bf985dc14aed05890aca343fdef38
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-11-01 12:25:42 +0530

    Update Favorites.jsx

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import PropertyCard from "../componnents/Cards/PropertyCard";
-import styled from "styled-components";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import PropertyCard from '../componnents/Cards/PropertyCard';
+import styled from 'styled-components';
 
 const FavoritesContainer = styled.div`
   padding: 30px;
@@ -23,16 +23,74 @@ const NoFavoritesMessage = styled.p`
   color: #999;
 `;
 
+const Title = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Desc = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const Location = styled.div`
+  width: fit-content;
+  font-size: 12px;
+  font-weight: 500;
+  padding: 2px 10px;
+  border-radius: 20px;
+  background: ${({ theme }) => theme.disabled + 50};
+`;
+
+const Price = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 20px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Strike = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_secondary + 60};
+  text-decoration: line-through;
+  text-decoration-color: ${({ theme }) => theme.text_secondary + 50};
+`;
+
+const Percent = styled.div`
+  font-size: 12px;
+  font-weight: 500;
+  color: green;
+`;
+
 const Favorites = () => {
   const favorites = useSelector((state) => state.favorites.list);
 
   return (
     <FavoritesContainer>
       <FavoritesHeading>Your Favorites</FavoritesHeading>
-      {favorites && favorites.length > 0 ? (
-        favorites.map((property) => <PropertyCard key={property._id} property={property} />)
+      {favorites && favorites.length === 0 ? (
+        <NoFavoritesMessage>You have no favorites yet!</NoFavoritesMessage>
       ) : (
-        <NoFavoritesMessage>You have no favorite properties yet.</NoFavoritesMessage>
+        favorites.map((property) => (
+          <PropertyCard key={property._id} property={property}>
+            <Title>{property?.title || "No title available"}</Title>
+            <Desc>{property?.desc || "No description available"}</Desc>
+            <Location>{property?.location || "No location specified"}</Location>
+            <Price>
+              ${property?.price?.org || "N/A"}
+              {property?.price?.mrp && <Strike>${property.price.mrp}</Strike>}
+              {property?.price?.off && <Percent>{property.price.off}% Off</Percent>}
+            </Price>
+          </PropertyCard>
+        ))
       )}
     </FavoritesContainer>
   );
